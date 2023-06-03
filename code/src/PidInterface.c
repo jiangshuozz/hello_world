95db56d1a7cf1e7438d9ed5e0f6c030bafa7a765
Shuo Jiang <34135398+jiangshuozz@users.noreply.github.com>
2023-06-03 16:28:00 +0800

    Update PidInterface.c

--- code/src/PidInterface.c
+++ code/src/PidInterface.c
@@ -3,11 +3,11 @@ unsigned int g_totalPidNum = 0;
 
 int AddNewProc()
 {
-  if (g_totalPidNum > 10) {
-    printf("AddNewProc fail!!");
+  if (g_totalPidNum >= 10) {
+    printf("\nAddNewProc fail!!");
     return -1;
   }
   
-  printf("PROC ID: %d init success!", g_totalPidNum++);
+  printf("\nPROC ID: %d init success!", g_totalPidNum++);
   return 0;
 }
